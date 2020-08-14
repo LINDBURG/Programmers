69de86f6ac564892db12c50120b477690f5fb317
LINDBURG <monc2170@gmail.com>
2020-08-14 18:14:30 +0900

    웨안댐

--- 탐욕법_섬 연결하기.py
+++ 탐욕법_섬 연결하기.py
@@ -1,19 +1,31 @@
 class Islands:
-    def __init__(self, n):
-        self.cost = 0
-        self.node = [10000 for i in range(n)]
-        self.movable = [dict() for i in range(n)]
+    def __init__(self):
+        self.answer = 0
+        self.node = [10000 for i in range(100)]
+        self.movable = [dict() for i in range(100)]
+        self.installed = [[] for i in range(100)]
         
     def costs(self, costs):
-        self.queue = [cost[0][0]]
         for i,j,cost in costs:
             self.movable[i][j] = cost
             self.movable[j][i] = cost
+        self.run(costs[0][0], 0)
         
+    def run(self, idx, val):
+        node = self.node
+        node[idx] = val
+        for key,val in self.movable[idx].items():
+            if key not in self.installed[idx] and val + node[idx] < node[key]:
+                self.installed[idx].append(key)
+                self.installed[key].append(idx)
+                self.answer += val
+                self.run(key, val + node[idx])
+            if key in self.installed[idx] and abs(node[idx] - node[key]) < val:
+                self.installed[idx].remove(key)
+                self.installed[key].remove(idx)
+                self.answer -= val
 
 def solution(n, costs):
-    answer = 0
-    islands = Islands(n)
+    islands = Islands()
     islands.costs(costs)
-    
-    return answer
+    return islands.answer
